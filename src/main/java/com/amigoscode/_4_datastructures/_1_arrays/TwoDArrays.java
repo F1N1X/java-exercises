9fa98c6474e1fd6a18d6d4a8d71472ec08ca9a0c
F1NiX <73650428+F1N1X@users.noreply.github.com>
2026-08-06 20:40:18 +0200

    Two-D Arrays

--- src/main/java/com/amigoscode/_4_datastructures/_1_arrays/TwoDArrays.java
+++ src/main/java/com/amigoscode/_4_datastructures/_1_arrays/TwoDArrays.java
@@ -3,11 +3,15 @@ package com.amigoscode._4_datastructures._1_arrays;
 // Exercise: Two-Dimensional Arrays
 // Learn how to create and work with 2D arrays (matrices) in Java.
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class TwoDArrays {
 
     public static void main(String[] args) {
 
         // TODO: 1 - Create a 3x3 int 2D array called 'matrix'
+        int[][] matrix = new int[3][3];
 
 
         // TODO: 2 - Fill the matrix with multiplication table values
@@ -18,21 +22,55 @@ public class TwoDArrays {
         //           3 6 9
 
 
+
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[0].length; j++)
+                matrix[i][j] = (i + 1) * (j + 1);
+
+
+        System.out.println(Arrays.deepToString(matrix));
+
+
         // TODO: 3 - Print the matrix in row/column format
         //           Each row on a new line, values separated by spaces or tabs
 
+        for (int[] value : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(" " + value[j]);
+            }
+            System.out.println();
+        }
 
         // TODO: 4 - Find and print the sum of all elements in the matrix
         //           Use nested loops to accumulate the total
+        int sum = 0;
+        for (int[] value : matrix)
+            for (int element : value)
+                sum += element;
+
+        System.out.println(sum);
 
 
         // TODO: 5 - Find and print the sum of row index 1 (the second row: 2, 4, 6)
         //           Iterate through all columns of that specific row
+        int rowSum = 0;
+        for (int[] value : matrix) {
+            for (int element : value) {
+                rowSum += element;
+            }
+            System.out.println("Sum: " + rowSum);
+            rowSum = 0;
+        }
 
 
         // TODO: 6 - Find and print the sum of the diagonal elements (top-left to bottom-right)
         //           Diagonal elements are where row index == column index
         //           Expected: matrix[0][0] + matrix[1][1] + matrix[2][2] = 1 + 4 + 9 = 14
 
+        int diagonalSum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            diagonalSum += matrix[i][i];
+        }
+        System.out.println("Diagonal sum: " + diagonalSum);
     }
 }
