8c8b438ba0f726d60d46363bfbdbe74ba16e56a4
F1NiX <73650428+F1N1X@users.noreply.github.com>
2026-08-27 22:06:56 +0200

    Exercise: Destinct

--- src/main/java/com/amigoscode/_7_streams/_4_duplicates/DistinctExercise.java
+++ src/main/java/com/amigoscode/_7_streams/_4_duplicates/DistinctExercise.java
@@ -80,6 +80,6 @@ public class DistinctExercise {
                 .distinct()
                 .collect(Collectors.toCollection( () -> new TreeSet<>(String.CASE_INSENSITIVE_ORDER)))
                 .forEach(System.out::println);
-
     }
+
 }
