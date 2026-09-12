404596b760d17a902ba2fae5d801fe70d6c2f5ba
F1NiX <73650428+F1N1X@users.noreply.github.com>
2026-09-12 13:49:04 +0200

    Exercise: Unbounded Wildcards (?)

--- src/main/java/com/amigoscode/_5_generics/_6_wildcards/UnboundedWildcard.java
+++ src/main/java/com/amigoscode/_5_generics/_6_wildcards/UnboundedWildcard.java
@@ -1,5 +1,6 @@
 package com.amigoscode._5_generics._6_wildcards;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,10 +18,16 @@ public class UnboundedWildcard {
     // TODO: 1 - Create a static method: void printList(List<?> list)
     //  It should iterate through the list and print each element.
     //  Note: elements come out as Object since the type is unknown.
+    public void printList(List<?> list) {
+        list.forEach(System.out::println);
+    }
 
 
     // TODO: 2 - Create a static method: int getSize(List<?> list)
     //  It should return the size of any list, regardless of its type.
+    public static int getSize(List<?> list) {
+        return list.size();
+    }
 
 
     public static void main(String[] args) {
@@ -30,16 +37,22 @@ public class UnboundedWildcard {
         //  (b) List<Integer> with values 1, 2, 3
         //  (c) List<Double> with values 1.1, 2.2, 3.3
         //  Call printList() and getSize() with each of them.
+        List<String> list = List.of("Hello","World");
+        List<Integer> numbers = List.of(1,2,3);
+        List<Double> doubles = List.of(1.0, 2.0, 3.3);
 
 
         // TODO: 4 - Demonstrate that you CANNOT add elements to a List<?>.
         //  Uncomment the code below, observe the compile error, then comment
         //  it back out. Add a comment explaining why adding is not allowed.
         //
-        // List<?> unknownList = Arrays.asList("a", "b", "c");
-        // unknownList.add("d");       // Why does this not compile?
+        //List<?> unknownList = Arrays.asList("a", "b", "c");
+        //unknownList.add("d");       // Why does this not compile?
         // unknownList.add(1);         // Why does this not compile either?
-
+        // List<?> means a list of an unknown element type.
+        // Since the compiler does not know the exact type,
+        // we cannot safely add a String, Integer, or any other value.
+        // We can only safely read elements as Object.
 
         // TODO: 5 - Demonstrate what you CAN do with List<?>:
         //  (a) Get the size
@@ -47,6 +60,11 @@ public class UnboundedWildcard {
         //  (c) Read elements as Object
         //  (d) Remove elements (by index or using clear())
         //  Write code showing at least two of these operations on a List<?>.
+        List<?> list2 = new ArrayList<String>(
+                Arrays.asList("a", "b", "c")
+        );
+
+        // List-Collections support these operations
 
     }
 }
