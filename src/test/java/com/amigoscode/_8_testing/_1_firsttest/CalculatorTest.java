a80ab062a9ef5f5d1e08e83472dadb3279206380
F1NiX <73650428+F1N1X@users.noreply.github.com>
2026-09-18 21:21:32 +0200

    Exercise: Your First Unit Tests

--- src/test/java/com/amigoscode/_8_testing/_1_firsttest/CalculatorTest.java
+++ src/test/java/com/amigoscode/_8_testing/_1_firsttest/CalculatorTest.java
@@ -3,7 +3,10 @@ package com.amigoscode._8_testing._1_firsttest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -13,6 +16,56 @@ import static org.junit.jupiter.api.Assertions.*;
  * Complete each TODO to practice basic assertions and test lifecycle annotations.
  */
 class CalculatorTest {
+
+    private Calculator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new Calculator();
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,2,3",
+            "4,5,9",
+            "10, 11, 21",
+    })
+    void add(int a, int b, int expected) {
+        int add = underTest.add(a, b);
+        assertThat(add).isEqualTo(expected);
+    }
+
+    @DisplayName("3 - 2 should equals 1")
+    @Test
+    void subtract3And2ResultShould1() {
+        //given
+        int a = 3;
+        int b = 2;
+        //when
+        int actual = underTest.subtract(a, b);
+        //then
+        assertThat(actual).isEqualTo(1);
+    }
+
+    @DisplayName("3 * 3 should equals 9")
+    @Test
+    void multiply() {
+        int a = 3;
+        int b = 3;
+
+        int actual = underTest.multiply(a, b);
+        assertThat(actual).isEqualTo(9);
+    }
+
+    @DisplayName("15 divide by 3 should 5")
+    @Test
+    void divide() {
+        int a = 15;
+        int b = 3;
+        int actual = underTest.divide(a, b);
+        assertThat(actual).isEqualTo(5);
+    }
 
     // TODO: 1 - Declare a Calculator field and use @BeforeEach to create a fresh
     //  Calculator instance before each test. This ensures tests are independent.
